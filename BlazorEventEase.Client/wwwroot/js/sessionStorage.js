aff27fbc7fdbb43797a099b65809c38d01e258a3
isologu <57639419+isologu@users.noreply.github.com>
2026-09-01 21:52:12 -0600

    Add files via upload

---
+++ BlazorEventEase.Client/wwwroot/js/sessionStorage.js
@@ -0,0 +1,11 @@
+﻿export function getItem(key) {
+    return window.sessionStorage.getItem(key);
+}
+
+export function setItem(key, value) {
+    window.sessionStorage.setItem(key, value);
+}
+
+export function removeItem(key) {
+    window.sessionStorage.removeItem(key);
+}
